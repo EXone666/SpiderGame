0d53b89995ac9b7673e8a67b646c9a6a3930111f
EXone666 <136240316+EXone666@users.noreply.github.com>
2023-06-23 16:53:36 +0300

    Add files via upload

---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.Designer.cs
@@ -0,0 +1,176 @@
+﻿namespace WindowsFormsApp3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblScore = new System.Windows.Forms.Label();
+            this.lblPoint = new System.Windows.Forms.Label();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.timer2 = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 450);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblScore
+            // 
+            this.lblScore.AutoSize = true;
+            this.lblScore.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblScore.Location = new System.Drawing.Point(668, 9);
+            this.lblScore.Name = "lblScore";
+            this.lblScore.Size = new System.Drawing.Size(44, 16);
+            this.lblScore.TabIndex = 1;
+            this.lblScore.Text = "Очки";
+            // 
+            // lblPoint
+            // 
+            this.lblPoint.AutoSize = true;
+            this.lblPoint.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lblPoint.Location = new System.Drawing.Point(718, 9);
+            this.lblPoint.Name = "lblPoint";
+            this.lblPoint.Size = new System.Drawing.Size(0, 16);
+            this.lblPoint.TabIndex = 2;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox2.Image")));
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(373, 187);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(54, 52);
+            this.pictureBox2.TabIndex = 3;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 10000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // timer2
+            // 
+            this.timer2.Interval = 1000;
+            this.timer2.Tick += new System.EventHandler(this.timer2_Tick);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox3.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox3.Image")));
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(532, 160);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(54, 52);
+            this.pictureBox3.TabIndex = 4;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox3_Click);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox4.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox4.Image")));
+            this.pictureBox4.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox4.InitialImage")));
+            this.pictureBox4.Location = new System.Drawing.Point(229, 306);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(54, 52);
+            this.pictureBox4.TabIndex = 5;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox4_Click);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.pictureBox5.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox5.Image")));
+            this.pictureBox5.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox5.InitialImage")));
+            this.pictureBox5.Location = new System.Drawing.Point(215, 110);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(54, 52);
+            this.pictureBox5.TabIndex = 6;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox5_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.lblPoint);
+            this.Controls.Add(this.lblScore);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblScore;
+        private System.Windows.Forms.Label lblPoint;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Timer timer2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+    }
+}
+
---
+++ WindowsFormsApp3/WindowsFormsApp3/Form1.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Rebar;
+
+
+namespace WindowsFormsApp3
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            random = new Random();
+        }
+        int score = 0;
+        bool click = false;
+        bool click1 = false;
+        bool click2 = false;
+        bool click3 = false;
+       
+      
+
+        private Random random;
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox2_Click(object sender, EventArgs e)
+        {
+            score++;
+            lblPoint.Text = score.ToString();
+           click = true;
+            pictureBox2.Visible = false;
+            pictureBox2.Visible = true;
+            int x = random.Next(0, ClientSize.Width - pictureBox2.Width);
+            int y = random.Next(0, ClientSize.Height - pictureBox2.Height);
+            pictureBox2.Location = new Point(x, y);
+           
+
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblPoint.Text = score.ToString();
+            timer1.Start();
+            timer2.Start();
+            
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            int time10 = 0;
+            time10++;
+            
+           
+            if(click == false)
+            {
+              
+                pictureBox2.Visible=false;
+                score--;
+                lblPoint.Text = score.ToString();
+                int x = random.Next(0, ClientSize.Width - pictureBox2.Width);
+                int y = random.Next(0, ClientSize.Height - pictureBox2.Height);
+                pictureBox2.Location = new Point(x, y);
+                pictureBox2.Visible = true;
+               
+            }
+            if (click1 == false)
+            {
+
+                pictureBox3.Visible = false;
+                score--;
+                lblPoint.Text = score.ToString();
+                int v = random.Next(0, ClientSize.Width - pictureBox3.Width);
+                int d = random.Next(0, ClientSize.Height - pictureBox3.Height);
+                pictureBox3.Location = new Point(v, d);
+                pictureBox3.Visible = true;
+            }
+            if (click2 == false)
+            {
+
+                pictureBox4.Visible = false;
+                score--;
+                lblPoint.Text = score.ToString();
+                int x = random.Next(0, ClientSize.Width - pictureBox4.Width);
+                int y = random.Next(0, ClientSize.Height - pictureBox4.Height);
+                pictureBox4.Location = new Point(x, y);
+                pictureBox4.Visible = true;
+            }
+            if (click3 == false)
+            {
+
+                pictureBox5.Visible = false;
+                score--;
+                lblPoint.Text = score.ToString();
+                int x = random.Next(0, ClientSize.Width - pictureBox5.Width);
+                int y = random.Next(0, ClientSize.Height - pictureBox5.Height);
+                pictureBox5.Location = new Point(x, y);
+                pictureBox5.Visible = true;
+            }
+            click = false;
+            click1= false;
+            click2= false;
+            click3= false;
+
+        }
+
+        private void timer2_Tick(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void pictureBox3_Click(object sender, EventArgs e)
+        {
+            score++;
+            lblPoint.Text = score.ToString();
+            click1 = true;
+            pictureBox3.Visible = false;
+            pictureBox3.Visible = true;
+            int v = random.Next(0, ClientSize.Width - pictureBox3.Width);
+            int d = random.Next(0, ClientSize.Height - pictureBox3.Height);
+            pictureBox3.Location = new Point(v, d);
+        }
+
+        private void pictureBox4_Click(object sender, EventArgs e)
+        {
+            score++;
+            lblPoint.Text = score.ToString();
+            click2 = true;
+            pictureBox4.Visible = false;
+            pictureBox4.Visible = true;
+            int x = random.Next(0, ClientSize.Width - pictureBox4.Width);
+            int y = random.Next(0, ClientSize.Height - pictureBox4.Height);
+            pictureBox4.Location = new Point(x, y);
+        }
+
+        private void pictureBox5_Click(object sender, EventArgs e)
+        {
+            score++;
+            lblPoint.Text = score.ToString();
+            click3 = true;
+            pictureBox5.Visible = false;
+            pictureBox5.Visible = true;
+            int x = random.Next(0, ClientSize.Width - pictureBox5.Width);
+            int y = random.Next(0, ClientSize.Height - pictureBox5.Height);
+            pictureBox5.Location = new Point(x, y);
+        }
+    }
+   }
+
